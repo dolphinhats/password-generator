824e9fb2ae32f439b70936013b6dd7e170c38041
dolphinhats <dptyler42@gmail.com>
2018-04-09 12:37:27 -0500

    Added the ability to set the hash function

--- generate.py
+++ generate.py
@@ -5,21 +5,22 @@ import time
 import sys
 import hashlib
 
+
 #todo Be more descriptive
 argparsev = argparse.ArgumentParser("Generates passwords")
-argparsev.add_argument("--master-pass", '-m', default=('%f' % time.time()), help='The master password for use in this program. Can be any string. Defaults to current time.time() when not specified.')
-argparsev.add_argument('--iterations', '-i', default=0, type=int, help='The number of additional iterations to perform. Defaults to 0 if not provided.')
-
+argparsev.add_argument("--master-pass", '-m', default=('%f' % time.time()), help='The master password for use in this program. Can be any string. (Default time.time())')
+argparsev.add_argument('--iterations', '-i', default=0, type=int, help='The number of additional iterations to perform. (Default %(default)s)')
+argparsev.add_argument('--hash-type','-a',choices=hashlib.algorithms_available, help="All hash functions available are those available via the hashlib library. The list can be found by running the python command 'hashlib.algorithms_available'. (Default %(default)s)", default="sha256")
 args = argparsev.parse_args(sys.argv[1:])
 
-print(args)
-
 #todo Choose hash type
-hash = hashlib.shs256()
+hash = hashlib.new(args.hash_type)
 
-hash.update(args.master_pass)
+hash.update(args.master_pass.encode())
 
 for i in range(args.iterations):
     hash.update(hash.digest())
 
 print(hash.hexdigest())
+
+
